18ab56fc2bf9be7e33e978f535ba0f768ea2d4d4
Cherfez <chervasev.leeuwen@gmail.com>
2020-04-15 16:50:03 +0200

    Hipster ipsum filler text for homepage. Basic. Links to /gyms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
+import "./styling/mobile.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Switch, Route } from "react-router-dom";
 
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,31 @@
 import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
-  return <div>IM HOME!</div>;
+  return (
+    <div>
+      <Jumbotron id="homepage">
+        <h1>MaxCap.</h1>
+      </Jumbotron>
+      <Container>
+        <h2>Welcome climber!</h2>
+        <p>
+          Prism la croix tote bag, chartreuse normcore lomo umami. Artisan
+          cardigan migas, austin 90's cronut stumptown vape tilde tousled forage
+          meggings. Tote bag banh mi sriracha mlkshk health goth, subway tile
+          letterpress fingerstache green juice 90's kickstarter tbh. Wayfarers
+          authentic fam helvetica waistcoat VHS disrupt PBR&B glossier sartorial
+          bushwick. Coloring book truffaut photo booth, kickstarter air plant
+          single-origin coffee aesthetic cray meh four loko. Squid messenger bag
+          ugh skateboard roof party. Bitters tacos gochujang migas kickstarter
+          bushwick. Tofu kale chips activated charcoal pork belly.{" "}
+        </p>
+        <Link to="/gyms">
+          <button>Search your gym!</button>
+        </Link>
+      </Container>
+    </div>
+  );
 }
